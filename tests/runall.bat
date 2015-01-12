39a022200dd8b7aa9bb5eb7d5ffeb67d9a2e2076
dkprice <dkprice@gmail.com>
2015-01-12 00:33:45 -0500

    Add an option to clean the test directory

--- tests/runall.bat
+++ tests/runall.bat
@@ -1,3 +1,22 @@
 @ECHO off
 REM This suite requires https://github.com/inkarkat/runVimTests to run
+
+if %1.==clean. goto Clean
+if %1.==run. goto Run
+
+echo "usage: runall.bat clean|run"
+goto End
+
+
+:Clean
+echo Cleaning
+del /Q *.out 2> nul
+del /Q *.msgout 2> nul
+goto End
+
+:Run
+echo Running
 ../../runVimTests/bin/runVimTests.cmd --pure vimgrep.suite
+goto End
+
+:End
